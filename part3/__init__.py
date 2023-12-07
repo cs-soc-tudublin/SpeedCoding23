41d0f77df397b965427e43ea9c9078aafd58fcf1
David Lynch <98290029+coillteoir@users.noreply.github.com>
2023-12-07 16:03:53 +0000

    init (#1)
    * init
    
    * time
    
    * tests added

---
+++ part3/__init__.py
@@ -0,0 +1,9 @@
+"""
+Find all the prime numbers in a list of integers
+Return a list of said prime numbers, preserve order
+If there are no primes, return an empty list
+"""
+
+
+def find_primes(nums: list[int]) -> list[int]:
+    return []
---
+++ part4/__init__.py
@@ -0,0 +1,9 @@
+"""
+Merge two sorted arrays of integers together.
+Return an array which is sorted
+No .sort methods, write your own algorithm
+"""
+
+
+def merge_ints(nums1: list[int], nums2: list[int]) -> list[int]:
+    return []
