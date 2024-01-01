6ab4439827c52e809d4992553e495a15a105b9bc
David <davite3@gmail.com>
2024-01-01 20:56:30 +0000

    part1

--- part1/__init__.py
+++ part1/__init__.py
@@ -4,4 +4,13 @@ Given a string, test if it is a palindrome or not
 
 
 def is_palindrome(data: str) -> bool:
-    return False
+    left = 0
+    right = 1
+
+    while left < len(data) / 2:
+        print(data[left], data[-right])
+        if data[left] != data[-right]:
+            return False
+        left += 1
+        right += 1
+    return True
